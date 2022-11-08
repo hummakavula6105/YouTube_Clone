9c9139aa4e8d39aeb47586637a3b72b333d8c75b
Danielle <hummakavula6105@gmail.com>
2022-11-08 18:37:12 -0500

    Created base Django project

--- backend/youtube_clone/youtube_clone/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from comment import Comment
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ['id', 'make', 'model', 'year', 'user_id']
-        depth = 1
